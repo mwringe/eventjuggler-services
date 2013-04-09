b6b55f9d02f7a384a892db82a6721079519f42c1
Stian Thorgersen <stian@redhat.com>
2013-04-09 12:45:03 +0100

    Added basic model for applications+providers and admin rest interfaces

---
+++ identity/identity-broker/src/main/java/org/eventjuggler/services/idb/model/IdentityProvider.java
@@ -0,0 +1,70 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2012, Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.eventjuggler.services.idb.model;
+
+import java.util.Map;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ * @author <a href="mailto:sthorger@redhat.com">Stian Thorgersen</a>
+ */
+@Entity
+@XmlRootElement
+public class IdentityProvider {
+
+    @OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    private Map<String, String> configuration;
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String provider;
+
+    public Map<String, String> getConfiguration() {
+        return configuration;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getProvider() {
+        return provider;
+    }
+
+    public void setConfiguration(Map<String, String> configuration) {
+        this.configuration = configuration;
+    }
+
+    public void setProvider(String provider) {
+        this.provider = provider;
+    }
+
+}
---
+++ identity/identity-broker/src/main/java/org/eventjuggler/services/idb/rest/AdminResource.java
@@ -0,0 +1,172 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2012, Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.eventjuggler.services.idb.rest;
+
+import java.util.List;
+
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+import javax.ws.rs.core.UriInfo;
+
+import org.eventjuggler.services.idb.model.Application;
+import org.eventjuggler.services.idb.model.IdentityProvider;
+
+/**
+ * @author <a href="mailto:sthorger@redhat.com">Stian Thorgersen</a>
+ */
+@Path("/admin")
+@Stateless
+public class AdminResource {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    @Inject
+    private KeyGenerator keyGenerator;
+
+    @POST
+    @Path("/applications")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response createApplication(Application application, @Context UriInfo uriInfo) {
+        if (application.getKey() != null || application.getSecret() != null) {
+            return Response.status(Status.BAD_REQUEST).build();
+        }
+
+        application.setKey(keyGenerator.createApplicationKey());
+        application.setSecret(keyGenerator.createApplicationSecret());
+
+        em.persist(application);
+
+        return Response.created(uriInfo.getAbsolutePathBuilder().build(application.getKey())).build();
+    }
+
+    @POST
+    @Path("/applications/{applicationKey}/providers")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response createProvider(@PathParam("applicationKey") String applicationKey, IdentityProvider provider,
+            @Context UriInfo uriInfo) {
+        if (provider.getId() != null) {
+            return Response.status(Status.BAD_REQUEST).build();
+        }
+
+        em.persist(provider);
+
+        return Response.created(uriInfo.getAbsolutePathBuilder().build(provider.getId())).build();
+    }
+
+    @DELETE
+    @Path("/applications/{applicationKey}")
+    public Response deleteApplication(@PathParam("applicationKey") String applicationKey) {
+        Application application = em.find(Application.class, applicationKey);
+        if (application == null) {
+            return Response.status(Status.NOT_FOUND).build();
+        }
+
+        em.remove(application);
+
+        return Response.noContent().build();
+    }
+
+    @DELETE
+    @Path("/applications/{applicationKey}/providers/{providerId}")
+    public Response deleteApplication(@PathParam("applicationKey") String applicationKey,
+            @PathParam("providerId") Long providerId) {
+        IdentityProvider provider = em.find(IdentityProvider.class, providerId);
+        if (provider == null) {
+            return Response.status(Status.NOT_FOUND).build();
+        }
+
+        em.remove(provider);
+
+        return Response.noContent().build();
+    }
+
+    @GET
+    @Path("/applications/{applicationKey}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getApplication(@PathParam("applicationKey") String applicationKey) {
+        Application application = em.find(Application.class, applicationKey);
+        if (application == null) {
+            return Response.status(Status.NOT_FOUND).build();
+        } else {
+            return Response.ok(application).build();
+        }
+    }
+
+    @GET
+    @Path("/applications")
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Application> getApplications() {
+        return em.createQuery("from Application", Application.class).getResultList();
+    }
+
+    @GET
+    @Path("/applications/{applicationKey}/providers/{providerId}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getProvider(@PathParam("applicationKey") String applicationKey, @PathParam("providerId") Long providerId) {
+        IdentityProvider provider = em.find(IdentityProvider.class, providerId);
+        if (provider == null) {
+            return Response.status(Status.NOT_FOUND).build();
+        } else {
+            return Response.ok(provider).build();
+        }
+    }
+
+    @GET
+    @Path("/applications/{applicationKey}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<IdentityProvider> getProviders(@PathParam("applicationKey") String applicationKey) {
+        return em.find(Application.class, applicationKey).getProviders();
+    }
+
+    @PUT
+    @Path("/applications/{applicationKey}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response updateApplication(@PathParam("applicationKey") String applicationKey, Application application) {
+        application = em.merge(application);
+        return Response.noContent().build();
+    }
+
+    @PUT
+    @Path("/applications/{applicationKey}/providers/{providerId}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response updateProvider(@PathParam("applicationKey") String applicationKey,
+            @PathParam("providerId") Long providerId, IdentityProvider provider) {
+        provider = em.merge(provider);
+        return Response.noContent().build();
+    }
+
+}
